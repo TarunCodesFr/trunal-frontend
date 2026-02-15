90abb059c3eda48abfa66c16ae87e6a2a5afe2f4
TarunCodes <akurax4@gmail.com>
2026-02-15 13:25:24 +0530

    fix2

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -82,7 +82,7 @@ export default function DashboardPage() {
 
     return (
         <SidebarProvider>
-            <AppSidebar user={user} projects={[]} />
+            <AppSidebar user={user} />
             <SidebarInset>
                 <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 sticky top-0 z-10">
                     <div className="flex items-center gap-2 px-4">
